894e97d8e4b710a1f9b04a662c8e543de8ac4b8e
LodeBo <l.bongaerts@telenet.be>
2025-11-28 14:27:51 +0100

    Bump Donut Chart version to 4.1.0

--- donut-chart.js
+++ donut-chart.js
@@ -1,5 +1,5 @@
 /*!
- * 🟢 Donut Chart v1.0.0
+ * 🟢 Donut Chart v4.1.0
  * Multi-segment donut (pie chart) for Home Assistant
  * - Multiple entities as segments
  * - Each segment: own color + value in the ring
@@ -11,7 +11,7 @@
 
 (() => {
   const TAG = "donut-chart";
-  const VERSION = "1.0.0";
+  const VERSION = "4.1.0";
 
   // Helpers to get LitElement / html / css for the editor
   const LitBase =
